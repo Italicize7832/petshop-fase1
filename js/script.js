fc1ea8e6a0d2c6149735b1b86106362c48853605
Italicize7832 <gabrieleduardo2693@gmail.com>
2025-12-08 23:47:25 -0300

    Atualiza index com estrutura completa e carrossel

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@
 
 // Script inicial
 console.log("Petshop Fase 2 - JS carregado");
+
+// Inicialização do carrossel
+document.addEventListener("DOMContentLoaded", () => {
+  const el = document.querySelector("#carouselPetshop");
+
+  // Verifica se o elemento existe e se o Bootstrap JS carregou
+  if (el && typeof bootstrap !== "undefined") {
+    new bootstrap.Carousel(el, {
+      interval: 3000,
+      ride: "carousel",
+      pause: false,
+      wrap: true
+    });
+  }
+});
